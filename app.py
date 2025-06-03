97bc2a594c604f930283bc1809d68deea2da5ea0
klvnd <klvinnd@student.ub.ac.id>
2025-06-04 06:45:17 +0700

    upp

--- app.py
+++ app.py
@@ -127,5 +127,9 @@ def update():
 
     return jsonify({'message': 'User face was updated successfully!', 'updated': True}), 200
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=5000, debug=True)
